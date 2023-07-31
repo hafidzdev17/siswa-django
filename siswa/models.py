54c9dc4718370a201f1c794d7717e87c7f0bd47b
hafidzdev17 <do.crazy192@gmail.com>
2023-07-31 13:59:12 +0700

    feat: update models

--- siswa/models.py
+++ siswa/models.py
@@ -4,9 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
     
 class Kelas(models.Model):
-    nama_kelas = models.CharField(max_length=10)
-    status_kelas = models.CharField(max_length=100)
 
+    STATUS_CHOICES = (
+        ('Aktif', 'Aktif'),
+        ('Nonaktif', 'Nonaktif'),
+    )
+
+    nama_kelas = models.CharField(max_length=10)
+    status_kelas = models.CharField(max_length=8, choices=STATUS_CHOICES, default='Aktif')
+    
     def __str__(self):
         return self.nama_kelas
 
@@ -14,8 +20,15 @@ class Kelas(models.Model):
         verbose_name_plural ="Kelas" 
 
 class Siswa(models.Model):
+
+    GENDER_CHOICES = (
+        ('L', 'Laki-laki'),
+        ('P', 'Perempuan'),
+    )
+    
     nisn = models.CharField(max_length=10, unique = True, null=False)
     nama = models.CharField(max_length=200, blank=False, null=False)
+    jenis_kelamin = models.CharField(max_length=1, choices=GENDER_CHOICES, default='L')
     alamat = models.CharField(max_length=200, blank=True, null=True)
     ttl = models.DateField(auto_now=False, auto_now_add=False)
     tahun_akademik = models.CharField(max_length=100)
@@ -42,12 +55,17 @@ class Pembayaran(models.Model):
         verbose_name_plural = "Pembayaran"
 
 class Petugas(models.Model):
-    
+
+    STATUS_CHOICES = (
+        ('Aktif', 'Aktif'),
+        ('Nonaktif', 'Nonaktif'),
+    )
+
     user = models.OneToOneField(User, blank =True, null=True, on_delete = models.CASCADE)
     nama_petugas = models.CharField(max_length=200, blank=True, null=False)
-    email = models.CharField(max_length=100,blank=True, null=False)
-    status = models.CharField(max_length=100,blank=True, null=False)
-    no_telpon = models.CharField(max_length=200, blank=True, null=False)
+    email = models.CharField(max_length=100,blank=True, unique = True, null=False)
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES, default='Aktif')
+    no_telpon = models.CharField(max_length=200, blank=True,  unique = True, null=False)
 
     def __str__(self):
         return self.nama_petugas
@@ -55,6 +73,33 @@ class Petugas(models.Model):
         verbose_name_plural = "Petugas"
 
 
+class RincianPembayaran(models.Model):
+
+    Category = (
+        ('KMS', 'KMS'),
+        ('LKS', 'LKS'),
+        ('Komputer', 'Komputer'),
+    )
+
+    Keterangan = (
+        ('Lunas', 'Lunas'),
+        ('Belum Lunas', 'Belum Lunas'),
+    )
+    
+    nama_siswa = models.ForeignKey(Siswa, blank=False, null=True, on_delete=models.SET_NULL)
+    kelas = models.ForeignKey(Kelas, blank=False, null=True, on_delete=models.SET_NULL)
+    kategori = models.CharField(max_length=150, blank=True, null=False, choices=Category)
+    tanggal = models.DateField(auto_now=False, auto_now_add=False)
+    biaya = models.CharField(max_length=100)
+    keterangan = models.CharField(max_length=150, blank=True, null=False, choices=Keterangan)
+    
+    def __str__(self):
+        return "%s" %(self.nama_siswa)
+
+    class Meta:
+        verbose_name_plural = "Rincian Pembayaran"
+
+    
 class Pelanggaran(models.Model):
     Category = (
         ('Ringan', 'Ringan'),
