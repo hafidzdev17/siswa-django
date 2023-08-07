03d3cdc88f1c06a5017ff8646f837bd1bc7132b4
hafidzdev17 <do.crazy192@gmail.com>
2023-08-07 21:11:45 +0700

    feat: update resource

--- siswa/resource.py
+++ siswa/resource.py
@@ -4,7 +4,10 @@ from import_export.fields import Field
 
 class PembayaranResource(resources.ModelResource):
     nama_siswa__nama = Field(attribute='nama_siswa', column_name='Nama Siswa')
+    nisn = Field(attribute='nama_siswa__nisn', column_name='NISN')
+    kelas = Field(attribute='nama_siswa__kelas__nama_kelas', column_name='Kelas')
+
     class Meta:
         model = Pembayaran
-        fields = ['nama_siswa__nama', 'jumlah_pembayaran', 'biaya_pembayaran', 'kategori_pembayaran', 'tanggal_pembayaran','tahun','keterangan']
+        fields = ['nama_siswa__nama', 'nisn', 'kelas','jumlah_pembayaran', 'biaya_pembayaran', 'kategori_pembayaran', 'tanggal_pembayaran','tahun','keterangan']
 
